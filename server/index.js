0fbba3760c6a041787ba43fca36995cb8d827e34
Seweryn Woźniak <sewerynwozniak111@gmail.com>
2024-01-28 23:25:51 +0100

    connecting to mongoDB

--- server/index.js
+++ server/index.js
@@ -3,9 +3,15 @@ require('dotenv').config();
 const {graphqlHTTP} = require('express-graphql');
 const { graphql } = require('graphql');
 const schema = require('./schema/schema')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000;
 
 const app = express()
+
+
+//Connect to database
+connectDB()
+
 
 app.use('/graphql', graphqlHTTP({
     schema,
