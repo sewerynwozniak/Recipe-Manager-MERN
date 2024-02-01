9068d813c3253b95ab9a3ffe2ac581504bfd86a5
Seweryn Woźniak <sewerynwozniak111@gmail.com>
2024-02-01 23:42:46 +0100

    adding mongoose models

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,8 @@
 const {projects, clients} = require('../sampleData.js')
 
+const Project =require('../models/Project.js')
+const Client =require('../models/Client.js')
+
 const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList} = require('graphql')
 
 
@@ -41,27 +44,27 @@ const RootQuery = new GraphQLObjectType({
         clients:{
             type: new GraphQLList(ClientType),
             resolve(parent, args){
-                return clients;
+                return Client.find()
             }
         },
         client:{
             type:ClientType,
             args:{id:{type:GraphQLID}},
             resolve(parent, args){
-                return clients.find(client=>client.id===args.id)
+                return Client.findById(parent.clientId)
             }
         },
         projects:{
             type: new GraphQLList(ProjectType),
             resolve(parent, args){
-                return projects;
+                return Project.find()
             }
         },
         project:{
             type:ProjectType,
             args:{id:{type:GraphQLID}},
             resolve(parent, args){
-                return projects.find(project=>project.id===args.id)
+                return Project.findById(args.id)
             }
         }
     }
